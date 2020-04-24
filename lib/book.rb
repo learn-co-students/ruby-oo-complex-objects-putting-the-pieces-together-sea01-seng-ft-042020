97d766f01ddb0abf13b715edd1be50108b785ce6
klarissasmith <ksmith_email@yahoo.com>
2020-04-24 14:52:19 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
+class Book
+    attr_reader :title
+    attr_writer
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    # def title
+    #     @title
+    # end
+
+    # def author=(author)
+    #     @author = author
+    # end
+
+    # def author
+    #     @author
+    # end
+
+    # def page_count=(number)
+    #         @page_count = number
+    # end
+
+    # def page_count
+    #     @page_count
+    # end
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+
+    # def genre
+    #     @genre
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
